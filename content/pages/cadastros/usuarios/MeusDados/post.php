8611bac685c7001691bf2fcd016e012c9322df45
David Lorena <david@virtualsolution.com.br>
2020-05-06 16:37:32 -0300

    Edição de dados Pessoais

---
+++ content/pages/cadastros/usuarios/MeusDados/post.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
+require_once __DIR__.'/../../../../../dist/php/general.inc.php';
+@session_start();
+if($_POST['action'] == 'PopularDadosUsuario'){
+    $id = $_SESSION['nobre_usuario_id'];
+    $sql = "SELECT * FROM usuarios WHERE id = '$id'";
+    $sql = $con->query($sql);
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($row);
+}
+if($_POST['action'] == 'SalvarDadosUsuario'){
+    $id = $_SESSION['nobre_usuario_id'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $sql = "UPDATE usuarios SET nome = :nome, email = :email WHERE id = :id";
+    $sql = $con->prepare($sql);
+    $sql->bindParam('nome', $nome);
+    $sql->bindParam('email', $email);
+    $sql->bindParam('id', $id);
+    $sql->execute();
+}
+exit;

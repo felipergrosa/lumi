2145f28b7a18396ca1a61442d8492fa9ddc2cb74
David Lorena <david@virtualsolution.com.br>
2020-05-16 18:29:17 -0300

    primeira e segunda etapas -> pedidos do sistema

--- content/pages/movimentacoes/pedidos/PedidosSistema/post.php
+++ content/pages/movimentacoes/pedidos/PedidosSistema/post.php
@@ -37,8 +37,10 @@ if(@$_POST['action'] == 'continuar'){
         exit;
     }
 
-    $sql = "SELECT a.* FROM clientes_empresas a
+    $sql = "SELECT a.*, b.nome as cliente_nome, c.nome_fantasia as empresa_nome
+    FROM clientes_empresas a
     LEFT JOIN clientes b ON a.cliente=b.id
+    LEFT JOIN empresas c ON a.empresa=c.id
     WHERE b.cpfcnpj = :cpfcnpj && a.empresa = :empresa";
     $sql = $con->prepare($sql);
     $sql->bindParam('cpfcnpj', $cpfcnpj);
@@ -61,6 +63,8 @@ if(@$_POST['action'] == 'continuar'){
     }
     else {
         $retorno['erro'] = 0;
+        $row = $sql->fetch(PDO::FETCH_ASSOC);
+        $retorno['dados'] = $row;
     }
 
 
@@ -134,35 +138,15 @@ if($_POST['action'] == 'SalvarDadosUsuario'){
     $sql->execute();
 }
 if($_POST['action'] == 'novo'){
-
-    $EnderecosFunc = new Enderecos($con);
     $Valida = new Valida();
+    $obrigatorios = Array('cpfcnpj', 'empresa');
 
-    $obrigatorios = array(
-        'nome'
-    );
-    $info['cadastro_clientes_edit_form_restricao'] = 0;
     $dados = $_POST['dados'];
     foreach ($dados as $dd) {
-        if($dd['name'] == "cadastro_clientes_edit_form_restricao"){
-            $dd['value'] = 1;
-        }
         if($dd['value'] == ""){
             $dd['value'] = null;
         }
         $info[$dd['name']] = $dd['value'];
-        $prefix_loop = "cadastro_clientes_edit_form_endereco_entrega";
-        if(substr($dd['name'], 0, strlen($prefix_loop)) == $prefix_loop){
-            $endereco_entrega[str_replace($prefix_loop.'_', "", $dd['name'])] = $dd['value'];
-        }
-        $prefix_loop = "cadastro_clientes_edit_form_endereco_cobranca";
-        if(substr($dd['name'], 0, strlen($prefix_loop)) == $prefix_loop){
-            $endereco_cobranca[str_replace($prefix_loop.'_', "", $dd['name'])] = $dd['value'];
-        }
-        $prefix_loop = "cadastro_clientes_edit_form_endereco";
-        if(substr($dd['name'], 0, strlen($prefix_loop)) == $prefix_loop){
-            $endereco_principal[str_replace($prefix_loop.'_', "", $dd['name'])] = $dd['value'];
-        }
 
     }
 
@@ -180,10 +164,7 @@ if($_POST['action'] == 'novo'){
         exit;
     }
 
-
     $info[$prefix.'cpfcnpj'] = trim(str_replace('.', '', str_replace('-', '', str_replace('/', '', $info[$prefix.'cpfcnpj']))));
-    $info[$prefix.'endereco_cep'] = str_replace('-', '', $info[$prefix.'endereco_cep']);
-
     $cpfcnpj = $info[$prefix.'cpfcnpj'];
     if(strlen($cpfcnpj) == 11){
         if(!$Valida->validaCPF($cpfcnpj)){
@@ -204,151 +185,84 @@ if($_POST['action'] == 'novo'){
         exit;
     }
 
+    $sql_cliente = "SELECT id FROM clientes WHERE cpfcnpj = :cpfcnpj";
+    $sql_cliente = $con->prepare($sql_cliente);
+    $sql_cliente->bindParam('cpfcnpj', $cpfcnpj);
+    $sql_cliente->execute();
 
+    $row_cliente = $sql_cliente->fetch(PDO::FETCH_ASSOC);
+    $info[$prefix.'cliente'] = $row_cliente['id'];
 
-
-
-    try {
-        $sql = "SELECT * FROM clientes WHERE cpfcnpj = :cpfcnpj";
-        $sql = $con->prepare($sql);
-        $sql->bindParam('cpfcnpj', $info[$prefix.'cpfcnpj']);
-        $sql->execute();
-    }
-    catch(Exception $e){
-        $error_message = $e->getMessage();
-        echo $error_message;
-        exit;
-    }
-    if($sql->rowCount() > 0){
-        echo 'CPF/CNPJ ja existe.';
-        exit;
-    }
-
-    $endereco_principal = $EnderecosFunc->novoEndereco($endereco_principal);
-    $endereco_entrega = $EnderecosFunc->novoEndereco($endereco_entrega);
-    $endereco_cobranca = $EnderecosFunc->novoEndereco($endereco_cobranca);
-
-    $sql = "INSERT INTO clientes
+    $sql = "INSERT INTO pedidos
         (
-            endereco,
-            endereco_cobranca,
-            endereco_entrega,
-            nome,
-            cpfcnpj,
-            rgie,
-            suframa,
-            suframa_validade,
-            desconto,
+            representante,
+            empresa,
+            cliente,
+            prioridade,
+            frete,
+            num_pedido_representante,
+            data_pedido,
+            data_previsao_faturamento,
+            num_pedido_compra,
+            tabela_preco,
+            cond_pagto,
+            transportadora,
+            desconto_padrao,
+            desconto_adic1,
+            desconto_adic2,
+            desconto_adic3,
+            desconto_part_com,
             natureza_operacao,
-            email1,
-            tel1,
-            fax,
-            site,
-            contato_compras,
-            contato_compras_fone,
-            contato_compras_email,
-            contato_cobranca,
-            contato_cobranca_fone,
-            contato_cobranca_email,
-            contato_cobranca_funcao,
-            grupo,
-            segmento_mercado,
-            regiao,
-            endereco_cobranca_contato,
-            endereco_cobranca_contato_telefone,
-            endereco_cobranca_contato_email,
-            endereco_entrega_cnpj,
-            endereco_entrega_ie,
-            endereco_entrega_contato,
-            endereco_entrega_contato_telefone,
-            endereco_entrega_contato_email,
-            observacoes,
-            atividade,
-            restricao,
-            restricao_texto,
-            data_cadastro
+            observacao,
+            data_cadastro,
+            usuario_cadastro
         )
 
         VALUES (
-                :endereco,
-                :endereco_cobranca,
-                :endereco_entrega,
-                :nome,
-                :cpfcnpj,
-                :rgie,
-                :suframa,
-                :suframa_validade,
-                :desconto,
-                :natureza_operacao,
-                :email1,
-                :tel1,
-                :fax,
-                :site,
-                :contato_compras,
-                :contato_compras_fone,
-                :contato_compras_email,
-                :contato_cobranca,
-                :contato_cobranca_fone,
-                :contato_cobranca_email,
-                :contato_cobranca_funcao,
-                :grupo,
-                :segmento_mercado,
-                :regiao,
-                :endereco_cobranca_contato,
-                :endereco_cobranca_contato_telefone,
-                :endereco_cobranca_contato_email,
-                :endereco_entrega_cnpj,
-                :endereco_entrega_ie,
-                :endereco_entrega_contato,
-                :endereco_entrega_contato_telefone,
-                :endereco_entrega_contato_email,
-                :observacoes,
-                :atividade,
-                :restricao,
-                :restricao_texto,
-                NOW()
+            1,
+            :empresa,
+            :cliente,
+            :prioridade,
+            :frete,
+            :num_pedido_representante,
+            :data_pedido,
+            :data_previsao_faturamento,
+            :num_pedido_compra,
+            :tabela_preco,
+            :cond_pagto,
+            :transportadora,
+            :desconto_padrao,
+            :desconto_adic1,
+            :desconto_adic2,
+            :desconto_adic3,
+            :desconto_part_com,
+            :natureza_operacao,
+            :observacao,
+            NOW(),
+            1
             )";
     try {
     $sql = $con->prepare($sql);
 
-
-
-    $sql->bindParam('endereco', $endereco_principal);
-    $sql->bindParam('endereco_cobranca', $endereco_cobranca);
-    $sql->bindParam('endereco_entrega', $endereco_entrega);
-    $sql->bindParam('nome', $info[$prefix.'nome']);
-    $sql->bindParam('cpfcnpj', $info[$prefix.'cpfcnpj']);
-    $sql->bindParam('rgie', $info[$prefix.'rgie']);
-    $sql->bindParam('suframa', $info[$prefix.'suframa']);
-    $sql->bindParam('suframa_validade', $info[$prefix.'suframa_validade']);
-    $sql->bindParam('desconto', $info[$prefix.'desconto']);
+    $sql->bindParam('empresa', $info[$prefix.'empresa']);
+    $sql->bindParam('cliente', $info[$prefix.'cliente']);
+    $sql->bindParam('prioridade', $info[$prefix.'prioridade']);
+    $sql->bindParam('frete', $info[$prefix.'frete']);
+    $sql->bindParam('num_pedido_representante', $info[$prefix.'num_pedido_representante']);
+    $sql->bindParam('data_pedido', $info[$prefix.'data_pedido']);
+    $sql->bindParam('data_previsao_faturamento', $info[$prefix.'data_previsao_faturamento']);
+    $sql->bindParam('num_pedido_compra', $info[$prefix.'num_pedido_compra']);
+    $sql->bindParam('tabela_preco', $info[$prefix.'tabela_preco']);
+    $sql->bindParam('cond_pagto', $info[$prefix.'cond_pagto']);
+    $sql->bindParam('transportadora', $info[$prefix.'transportadora']);
+    $sql->bindParam('desconto_padrao', $info[$prefix.'desconto_padrao']);
+    $sql->bindParam('desconto_adic1', $info[$prefix.'desconto_adic1']);
+    $sql->bindParam('desconto_adic2', $info[$prefix.'desconto_adic2']);
+    $sql->bindParam('desconto_adic3', $info[$prefix.'desconto_adic3']);
+    $sql->bindParam('desconto_part_com', $info[$prefix.'desconto_part_com']);
     $sql->bindParam('natureza_operacao', $info[$prefix.'natureza_operacao']);
-    $sql->bindParam('email1', $info[$prefix.'email1']);
-    $sql->bindParam('tel1', $info[$prefix.'tel1']);
-    $sql->bindParam('fax', $info[$prefix.'fax']);
-    $sql->bindParam('site', $info[$prefix.'site']);
-    $sql->bindParam('contato_compras', $info[$prefix.'contato_compras']);
-    $sql->bindParam('contato_compras_fone', $info[$prefix.'contato_compras_fone']);
-    $sql->bindParam('contato_compras_email', $info[$prefix.'contato_compras_email']);
-    $sql->bindParam('contato_cobranca', $info[$prefix.'contato_cobranca']);
-    $sql->bindParam('contato_cobranca_fone', $info[$prefix.'contato_cobranca_fone']);
-    $sql->bindParam('contato_cobranca_email', $info[$prefix.'contato_cobranca_email']);
-    $sql->bindParam('contato_cobranca_funcao', $info[$prefix.'contato_cobranca_funcao']);
-    $sql->bindParam('grupo', $info[$prefix.'grupo']);
-    $sql->bindParam('segmento_mercado', $info[$prefix.'segmento_mercado']);
-    $sql->bindParam('regiao', $info[$prefix.'regiao']);
-    $sql->bindParam('endereco_cobranca_contato', $info[$prefix.'endereco_cobranca_contato']);
-    $sql->bindParam('endereco_cobranca_contato_telefone', $info[$prefix.'endereco_cobranca_contato_telefone']);
-    $sql->bindParam('endereco_cobranca_contato_email', $info[$prefix.'endereco_cobranca_contat_email']);
-    $sql->bindParam('endereco_entrega_cnpj', $info[$prefix.'endereco_entrega_cnpj']);
-    $sql->bindParam('endereco_entrega_ie', $info[$prefix.'endereco_entrega_ie']);
-    $sql->bindParam('endereco_entrega_contato', $info[$prefix.'endereco_entrega_contato']);
-    $sql->bindParam('endereco_entrega_contato_telefone', $info[$prefix.'endereco_entrega_contato_telefone']);
-    $sql->bindParam('endereco_entrega_contato_email', $info[$prefix.'endereco_entrega_contato_email']);
-    $sql->bindParam('observacoes', $info[$prefix.'observacoes']);
-    $sql->bindParam('restricao', $info[$prefix.'restricao']);
-    $sql->bindParam('restricao_texto', $info[$prefix.'restricao_texto']);
-    $sql->bindParam('atividade', $info[$prefix.'atividade']);
+    $sql->bindParam('observacao', $info[$prefix.'observacao']);
+
     $sql->execute();
 
     }
@@ -358,19 +272,6 @@ if($_POST['action'] == 'novo'){
         exit;
     }
 
-    $insertId = $con->lastInsertId();
-
-    @$empresas = $_POST['empresas'];
-
-    if(@is_array($empresas)){
-        foreach($empresas as $ep){
-            $sql = "INSERT INTO clientes_empresas (cliente, empresa) VALUES (:cliente, :empresa)";
-            $sql = $con->prepare($sql);
-            $sql->bindParam('cliente', $insertId);
-            $sql->bindParam('empresa', $ep);
-            $sql->execute();
-        }
-    }
     echo 0;
     exit;
 

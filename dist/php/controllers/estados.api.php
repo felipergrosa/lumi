b11b8273b81d33f2a1b003aee946b966ff9a090e
david2050 <david@virtualsolution.com.br>
2021-01-14 16:50:40 -0300

    estados do SQL

--- dist/php/controllers/estados.api.php
+++ dist/php/controllers/estados.api.php
@@ -2,21 +2,23 @@
 header('Access-Control-Allow-Origin: *');
 session_start();
 require __DIR__.'/../general.inc.php';
-
+$con_sql_server = new PDO ("dblib:host=$mssql_hostname;dbname=$mssql_dbname", "$mssql_username", "$mssql_pw");
 if(@$_POST['action'] == 'GetEstados'){
-    $sql = "SELECT * FROM dados_estados ORDER BY sigla ASC";
-    $sql = $con->query($sql);
+    $sql = "SELECT Estado FROM BusinessCadMunicipio WHERE Estado != '' GROUP BY Estado ORDER BY Estado ";
+    $sql = $con_sql_server->query($sql);
     $row = $sql->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode(utf8ize($row));
     exit;
 }
 if(@$_POST['action'] == 'GetEstadoSigla'){
     $sigla = $_POST['sigla'];
-    $sql = "SELECT id FROM dados_estados WHERE sigla = :sigla";
-    $sql = $con->prepare($sql);
-    $sql->bindParam('sigla', $sigla);
+    $sql = "SELECT Estado FROM BusinessCadMunicipio WHERE Estado != '' GROUP BY Estado ORDER BY Estado ";
+    $sql = $con_sql_server->prepare($sql);
+    // $sql->bindParam('sigla', $sigla);
     $sql->execute();
-    $row = $sql->fetch(PDO::FETCH_ASSOC);
-    echo $row['id'];
+    $row = $sql->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode(utf8ize($row));
+    exit;
+    // echo $row['id'];
     exit;
 }

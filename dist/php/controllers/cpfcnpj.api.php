42b0f920ee3e91592bbe26cf770d7ab3fa7e02e5
david2050 <david@virtualsolution.com.br>
2021-01-20 16:41:27 -0300

    cadastro de pedidos -> busca de cliente por CNPJCPF em SQL e população de campos

--- dist/php/controllers/cpfcnpj.api.php
+++ dist/php/controllers/cpfcnpj.api.php
@@ -4,6 +4,7 @@ session_start();
 require __DIR__.'/../general.inc.php';
 
 if(@$_POST['action'] == 'valida'){
+    $continuar = false;
     $Valida = new Valida();
     // var_dump($_POST['cpfcnpj']);
     $cpfcnpj = $_POST['cpfcnpj'];
@@ -17,6 +18,7 @@ if(@$_POST['action'] == 'valida'){
         else {
             $resultado['erro'] = 0;
             $resultado['resposta'] = 'CPF Validado';
+            $continuar = true;
         }
     }
     elseif(strlen($cpfcnpj) == 14){
@@ -27,12 +29,30 @@ if(@$_POST['action'] == 'valida'){
         else {
             $resultado['erro'] = 0;
             $resultado['resposta'] = 'CNPJ Validado';
+            $continuar = true;
+
         }
 
     }
     else {
         $resultado['erro'] = 1;
         $resultado['resposta'] = 'Formato CPF/CNPJ Inválido';
+    }
+
+    if($continuar){
+        //$con_sql_server
+        $sql = "SELECT * FROM BusinessCadCliente WHERE Cnpj_Cnpf = :cpfcnpj";
+        $sql = $con_sql_server->prepare($sql);
+        $sql->bindParam('cpfcnpj', $_POST['cpfcnpj']);
+        $sql->execute();
+        $row = $sql->fetch(PDO::FETCH_ASSOC);
+        if($row){
+            $resultado['existe'] = 1;
+            $resultado['dados'] = $row;
+        }
+        else {
+            $resultado['existe'] = 0;
+        }
     }
 
     echo json_encode($resultado);

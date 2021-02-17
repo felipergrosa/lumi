2bb56ea0f928dcdb8d3ecfab3775d2887e570506
david2050 <david@virtualsolution.com.br>
2021-02-17 13:35:52 -0300

    api de busca de produtos por ID

--- dist/php/controllers/produtos.api.php
+++ dist/php/controllers/produtos.api.php
@@ -19,3 +19,21 @@ if($_POST['action'] == "busca_produtos"){
     }
     exit;
 }
+
+if($_POST['action'] == "busca_produtos_id"){
+    $tabela = $_POST['tabela'];
+    $busca = $_POST['busca'];
+    $con_sql_server = new PDO ("dblib:host=$mssql_hostname;dbname=$mssql_dbname", "$mssql_username", "$mssql_pw");
+    $sql = new SqlServer($con_sql_server);
+    $busca = $sql->BuscaProduto($busca, $tabela, true, 5);
+    if(@$busca[0]){
+        $retorno['fetch'] = $busca;
+        $retorno['status'] = 'ok';
+        echo json_encode(utf8ize($retorno));
+    }
+    else {
+        $retorno['status'] = 'erro';
+        echo json_encode(utf8ize($retorno));
+    }
+    exit;
+}

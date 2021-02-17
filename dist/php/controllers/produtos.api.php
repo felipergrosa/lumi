a1a3b9dfafa2795f8b8066acee51a1dd7d31d7f4
david2050 <david@virtualsolution.com.br>
2021-02-17 16:32:09 -0300

    exibicao de foto de produto no pedido

--- dist/php/controllers/produtos.api.php
+++ dist/php/controllers/produtos.api.php
@@ -3,13 +3,30 @@ header('Access-Control-Allow-Origin: *');
 session_start();
 require __DIR__.'/../general.inc.php';
 if($_POST['action'] == "busca_produtos"){
+    $foto_dir = __DIR__.'/../../../content/pages/cadastros/cadastros/CadastroProdutos/docs/';
+    $relative_foto_dir = "content/pages/cadastros/cadastros/CadastroProdutos/docs/";
     $tabela = $_POST['tabela'];
     $busca = $_POST['busca'];
     $con_sql_server = new PDO ("dblib:host=$mssql_hostname;dbname=$mssql_dbname", "$mssql_username", "$mssql_pw");
     $sql = new SqlServer($con_sql_server);
     $busca = $sql->BuscaProduto($busca, $tabela, false, 5);
     if(@$busca[0]){
-        $retorno['fetch'] = $busca;
+        foreach($busca as $key=>$val){
+            $nova_busca[$key] = $busca[$key];
+            $imagem = $val['empresa'].'-'.$val['produto_id'].'.jpg';
+            $arquivo_imagem_local = $foto_dir.$imagem;
+            // echo $arquivo_imagem_local;
+            if(@file_exists($arquivo_imagem_local)){
+                $nova_busca[$key]['produto_imagem'] = '<center><img src="'.$relative_foto_dir.$imagem.'" style="max-width: 200px;"/></center>';
+                // $nova_busca[$key]['produto_imagem'] = '<center><img src="'.$relative_foto_dir.'01-01.016-0.jpg" style="max-width: 200px;"/></center>';
+
+            }
+            else {
+                $nova_busca[$key]['produto_imagem'] = '';
+
+            }
+        }
+        $retorno['fetch'] = $nova_busca;
         $retorno['status'] = 'ok';
         echo json_encode(utf8ize($retorno));
     }
